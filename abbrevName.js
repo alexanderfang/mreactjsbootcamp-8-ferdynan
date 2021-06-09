79dde706b7262f77d23f74daa72c1a2c70be82aa
Ferdynan <alexanderfang84@gmail.com>
2021-06-09 22:54:59 +0700

    abbreviation name

---
+++ abbrevName.js
@@ -0,0 +1,15 @@
+/*
+    Buatlah nama yg ada pada variable menjadi nama singkatan dan harus berubah menjadi huruf besar
+    contoh : 
+    Sam Harris maka akan menjadi SH
+*/ 
+let name = "Sam Harris"
+let arr = name.replace(/\s+/g,' ').trim().split(' ')
+let result = ''
+
+for (let i = 0; i < arr.length; i++) {
+    if(arr[i][0])
+        result += arr[i][0].toUpperCase()
+}
+
+console.log(result)
