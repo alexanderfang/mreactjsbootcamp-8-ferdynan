494b1ed13370ddaf6304409d9d966dc81721d48b
Ferdynan <alexanderfang84@gmail.com>
2021-06-09 23:01:53 +0700

    update abbrev name

--- abbrevName.js
+++ abbrevName.js
@@ -3,13 +3,20 @@
     contoh : 
     Sam Harris maka akan menjadi SH
 */ 
+// let name = "Sam Harris"
+// let arr = name.replace(/\s+/g,' ').trim().split(' ')
+// let result = ''
+
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i][0])
+//         result += arr[i][0].toUpperCase()
+// }
+
+// console.log(result)
 let name = "Sam Harris"
-let arr = name.replace(/\s+/g,' ').trim().split(' ')
-let result = ''
-
-for (let i = 0; i < arr.length; i++) {
-    if(arr[i][0])
-        result += arr[i][0].toUpperCase()
-}
-
-console.log(result)
+let arr = name.replace(/\s+/g,' ').trim().split(' ').map(function(item){
+    if(item[0])
+        return item[0].toUpperCase()
+    return ''
+})
+console.log(arr.join(''))
